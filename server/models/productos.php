ab707206e0f63592d965885fe77a5716db231102
toninence <toninence@gmail.com>
2021-07-14 15:58:36 -0300

    front test

--- server/models/productos.php
+++ server/models/productos.php
@@ -3,13 +3,13 @@ function mostrar_productos() {
     return json_encode([
         [
             "nombre"=> "Coca cola", 
-            "precio" => 50, 
-            "Cantidad"=>10
+            "precio" => 180, 
+            "cantidad"=>10
         ],
         [
-            "nombre"=> "Coca cola", 
-            "precio" => 50, 
-            "Cantidad"=>5 
+            "nombre"=> "La Gotita", 
+            "precio" => 150, 
+            "cantidad"=>5 
         ]
     ]    
     );
